09ec4b3728dd240825dae231d61fa5f622418799
Novirius <professional.nt7@gmail.com>
2020-01-20 17:03:03 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+// You can now delete our 'Hello World' route
+app.use(passport.initialize());
+require('./config/passport')(passport);
+//Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//Tell Express to use your newly imported routes
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
